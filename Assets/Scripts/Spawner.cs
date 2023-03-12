0ac128779bdfbde378caf4628a0502330e013223
NikaBang <72190298+NikaBang@users.noreply.github.com>
2023-03-12 12:24:21 +0500

    commite

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
-public class Spawner : MonoBehaviour
+public class Spawner : ObjectPool
 {
     [SerializeField] private GameObject _enemyPrefab;
     [SerializeField] private Transform[] _spawnPoints;
@@ -10,16 +11,31 @@ public class Spawner : MonoBehaviour
 
     private float _elapsedTime = 0;
 
+    private void Start()
+    {
+        Initialize(_enemyPrefab);
+    }
+
     private void Update()
     {
         _elapsedTime += Time.deltaTime;
 
         if (_elapsedTime >= _secondBetweenSpawn)
         {
-            _elapsedTime = 0;
+            if (TryGetObject(out GameObject enemy))
+            {
+                _elapsedTime = 0;
 
-            int spawnPointRandom = Random.Range(0, _spawnPoints.Length);
-            Instantiate(_enemyPrefab, _spawnPoints[spawnPointRandom]);
+                int spawnPointRandom = Random.Range(0, _spawnPoints.Length);
+
+                SetEnemy(enemy, _spawnPoints[spawnPointRandom].position);
+            }
         }
+    }
+
+    private void SetEnemy(GameObject enemy, Vector3 spawnPoint)
+    {
+        enemy.SetActive(true);
+        enemy.transform.position = spawnPoint;
     }
 }
